ef1fe35eb84997b1157eb797b55a7aa829832f44
INotSleep <77581131+INotSleep@users.noreply.github.com>
2022-03-30 22:18:42 +0300

    Update auto.js

--- src/auto.js
+++ src/auto.js
@@ -1,15 +1,15 @@
 const generateGame = require("./generateGame.js");
-const { getDB, findChannel, removeItem, get } = require("database.js");
+const { getDB, findChannel, removeItem, get } = require("./database.js");
 
 module.exports = function startAuto(client) {
   setInterval(async() => {
     var db = getDB()
     for (var item of db ) {
-      var channel = await client.channels.fetch(channelId).catch(e => {
-        removeItem(get(findChannel(channelId)));
-        continue;
-      }
-      channel.send(generateGame(db.inputs[0], db.inputs[1], db.inputs[2], false, db.inputs[3]))
+      var channel = await client.channels.fetch(item.channelId).catch(e => {
+        removeItem(get(findChannel(item.channelId)));
+      })
+      if (!channel) return;
+	  channel.send(generateGame(item.inputs[0], item.inputs[1], item.inputs[2], false, item.inputs[3]))
     }
   }, 60*1000);
 }
